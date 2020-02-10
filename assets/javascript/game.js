791dcc015d13977ff9bca95c01787ad1826aa226
Shayda Bakhshi <shayda.bakhshi@gmail.com>
2020-02-09 19:51:11 -0600

    added media queries; fixed hint image bugs

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,9 @@
 // Here are our Skyrim-themed word options for the game:
-var wordOptions = ["dragonborn", "spriggan", "sweetroll", "knee", "dragon", "daedra", "vampire", "dog", "map"];
+var wordOptions = ["dragonborn", "spriggan", "sweetrolls", "knee", "dragon", "daedra", "vampire", "dog", "map", "mage", "talos", "lydia"];
 
+// ********************************************
+// This will generate a random word for the game, but we want to go in order to avoid repeats
+// ********************************************
 
 // This is where we'll select a word for the current game session:
 function getWord() {
@@ -9,29 +12,18 @@ function getWord() {
   return randomWord;
 }
 
-// We'll show an image hint once the word is chosen
-// function showHint(hint) {
-//   if (hint === "dragonborn") {
-//     document.getElementById("hint-image").innerHTML = '<img src="assets/images/dragonborn.png" class="hint-image" alt="">';
-//   } else if (hint === "spriggan") {
-//       document.getElementById("hint-image").innerHTML = '<img src="assets/images/spriggan.png" class="hint-image" alt="">';
-//   } else if (hint === "sweetroll") {
-//       document.getElementById("hint-image").innerHTML = '<img src="assets/images/sweetroll.png" class="hint-image" alt="">';
-//   } else if (hint === "knee") {
-//       document.getElementById("hint-image").innerHTML = '<img src="assets/images/knee.png" class="hint-image" alt="">';
-//   } else if (hint === "dragon") {
-//       document.getElementById("hint-image").innerHTML = '<img src="assets/images/dragon.png" class="hint-image" alt="">';
-//   } else if (hint === "daedra") {
-//       document.getElementById("hint-image").innerHTML = '<img src="assets/images/dragonborn.png" class="hint-image" alt="">';
-//   } else if (hint === "vampire") {
-//       document.getElementById("hint-image").innerHTML = '<img src="assets/images/dragonborn.png" class="hint-image" alt="">';
-//   } else if (hint === "dog") {
-//       document.getElementById("hint-image").innerHTML = '<img src="assets/images/dragonborn.png" class="hint-image" alt="">';
-//   } else if (hint === "map") {
-//       document.getElementById("hint-image").innerHTML = '<img src="assets/images/dragonborn.png" class="hint-image" alt="">';
+
+// ********************************************
+// NEED TO FIX: I want to loop through the wordOptions array but this terminates after one loop
+// the way it's currently written
+// ********************************************
+// function getWord() {
+//   for (var i = 0; i < wordOptions.length; i++) {
+//     var currentWord = wordOptions[i];
+//     console.log(currentWord);
+//     return currentWord;
 //   }
 // }
-
 
 // Once we have our word, we'll run this function to convert it to blank spaces
 function displayWordSpaces(x) {
@@ -52,30 +44,41 @@ function displayWordSpaces(x) {
     // console.log(spaceArray);
   }
 
-  if (displayWord === "dragonborn") {
-    document.getElementById("hint-image").innerHTML = '<img src="assets/images/dragonborn.png" class="hint-image" alt="">';
-  } else if (displayWord === "spriggan") {
-      document.getElementById("hint-image").innerHTML = '<img src="assets/images/spriggan.png" class="hint-image" alt="">';
-  } else if (displayWord === "sweetroll") {
-      document.getElementById("hint-image").innerHTML = '<img src="assets/images/sweetroll.png" class="hint-image" alt="">';
-  } else if (displayWord === "knee") {
-      document.getElementById("hint-image").innerHTML = '<img src="assets/images/knee.png" class="hint-image" alt="">';
-  } else if (displayWord === "dragon") {
-      document.getElementById("hint-image").innerHTML = '<img src="assets/images/dragon.png" class="hint-image" alt="">';
-  } else if (displayWord === "daedra") {
-      document.getElementById("hint-image").innerHTML = '<img src="assets/images/dragonborn.png" class="hint-image" alt="">';
-  } else if (displayWord === "vampire") {
-      document.getElementById("hint-image").innerHTML = '<img src="assets/images/dragonborn.png" class="hint-image" alt="">';
-  } else if (displayWord === "dog") {
-      document.getElementById("hint-image").innerHTML = '<img src="assets/images/dragonborn.png" class="hint-image" alt="">';
-  } else if (displayWord === "map") {
-      document.getElementById("hint-image").innerHTML = '<img src="assets/images/dragonborn.png" class="hint-image" alt="">';
-  }
-
   // This will write the space array to the html document in the proper place.
   // The .join("") method will eliminate commas within the array so that only spaces are printed.
   document.getElementById("current-word").innerHTML = spaceArray.join("");
+
+  // We'll show an image hint once the word is chosen
+  if (displayWord === "dragonborn") {
+    document.getElementById("hint-image").innerHTML = '<img src="assets/images/dragonborn.png" class="hint-image" alt="">';
+  } else if (displayWord === "spriggan") {
+    document.getElementById("hint-image").innerHTML = '<img src="assets/images/spriggan.png" class="hint-image" alt="">';
+  } else if (displayWord === "sweetrolls") {
+    document.getElementById("hint-image").innerHTML = '<img src="assets/images/sweetrolls.png" class="hint-image" alt="">';
+  } else if (displayWord === "knee") {
+    document.getElementById("hint-image").innerHTML = '<img src="assets/images/knee.png" class="hint-image" alt="">';
+  } else if (displayWord === "dragon") {
+    document.getElementById("hint-image").innerHTML = '<img src="assets/images/dragon.png" class="hint-image" alt="">';
+  } else if (displayWord === "daedra") {
+    document.getElementById("hint-image").innerHTML = '<img src="assets/images/daedra.png" class="hint-image" alt="">';
+  } else if (displayWord === "vampire") {
+    document.getElementById("hint-image").innerHTML = '<img src="assets/images/vampire.png" class="hint-image" alt="">';
+  } else if (displayWord === "dog") {
+    document.getElementById("hint-image").innerHTML = '<img src="assets/images/dog.png" class="hint-image" alt="">';
+  } else if (displayWord === "map") {
+    document.getElementById("hint-image").innerHTML = '<img src="assets/images/map.png" class="hint-image" alt="">';
+  } else if (displayWord === "mage") {
+    document.getElementById("hint-image").innerHTML = '<img src="assets/images/mage.png" class="hint-image" alt="">';
+  } else if (displayWord === "talos") {
+    document.getElementById("hint-image").innerHTML = '<img src="assets/images/talos.png" class="hint-image" alt="">';
+  } else if (displayWord === "lydia") {
+    document.getElementById("hint-image").innerHTML = '<img src="assets/images/lydia.png" class="hint-image" alt="">';
+  }
+
 }
+
+
+
 
 // Once we have word length, the user will begin guessing
 // function userGuesses() {
